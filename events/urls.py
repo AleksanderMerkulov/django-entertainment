877ecbff500625f93f1f79ecbc84e0bef2cef424
Alex <merkulovwork@mail.ru>
2023-11-26 21:13:50 +1000

    проработал все основные моменты - осталось продумать дальнейшую логику

--- events/urls.py
+++ events/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.events_all, name="events_all"),
+    path('<int:pk>', views.EventDetail.as_view(), name="event_detail"),
 ]
--- events/views.py
+++ events/views.py
@@ -1,5 +1,16 @@
 from django.shortcuts import render
+from .models import Event
+from django.views.generic import DetailView
+
 
 # Create your views here.
 def events_all(request):
-    return render(request, 'events/events_all.html')
+    events = Event.objects.all()
+
+    return render(request, 'events/events_all.html', {"events": events})
+
+
+class EventDetail(DetailView):
+    model = Event
+    template_name = 'events/event_detail.html'
+    context_object_name = 'event'
