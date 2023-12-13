553be4a2e7c2909485fe097101c2dd8681137d7e
Меркулов Александр Андреевич [012739] <merkulovwork@mail.ru>
2023-12-13 18:34:55 +1000

    описал отображение событий по категориям (теперь есть необзодимость устанавливать пакет Pillow)

--- account/views.py
+++ account/views.py
@@ -1,9 +1,23 @@
+import django.utils.timezone
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
+
+from events.models import Event
 
 
 # Create your views here.
 @login_required
 def account(request):
-    return render(request, 'account/account_main.html')
+    future_event = Event.objects.filter(creator_id=request.user.id) & Event.objects.filter(
+        date__lt=django.utils.timezone.now())
+
+    today_event = Event.objects.filter(creator_id=request.user.id) & Event.objects.filter(
+        date__day=django.utils.timezone.now().day)
+
+    context = {
+        'future_event': future_event,
+        'today_event': today_event
+    }
+
+    return render(request, 'account/account_main.html', context)
     pass
